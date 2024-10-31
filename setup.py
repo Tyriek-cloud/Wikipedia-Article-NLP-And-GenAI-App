21afdb0ca64cb15a3b7107791527277463965c21
Tyriek Warren <62261407+Tyriek-cloud@users.noreply.github.com>
2024-10-31 19:44:49 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# Install requirements
-subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
-
-# Download spaCy model
-subprocess.run(['python', '-m', 'spacy', 'download', 'en_core_web_sm'])
