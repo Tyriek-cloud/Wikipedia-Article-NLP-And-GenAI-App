9cd0dc3c0531dd719e906613f5f947dbad53e98a
Tyriek Warren <62261407+Tyriek-cloud@users.noreply.github.com>
2024-10-31 11:14:50 -0400

    Update app.py
    Added error handling.

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ def extract_images(url):
 
 # Call Function to communicate with my Wit.ai app
 def wit_ai_response(message):
-    access_token = 'WIT_AI_ACCESS_TOKEN'
+    access_token = os.getenv('WIT_AI_ACCESS_TOKEN')
+    if not access_token:
+        return 'Error: Access token is not set.'
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
