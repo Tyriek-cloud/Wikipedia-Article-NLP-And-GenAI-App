50d169645d44117d671ca922d19241f2e709e667
Tyriek Warren <62261407+Tyriek-cloud@users.noreply.github.com>
2024-11-20 20:31:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Manually specify the version of ChromeDriver
+driver = webdriver.Chrome(executable_path=ChromeDriverManager(version="your-chrome-version").install(), options=chrome_options)
 
 # Download NLTK data
 nltk.download('punkt')
